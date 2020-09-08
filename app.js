c2392a7ba7e2f39c9c189ad0ca52234be531c58f
Paul Prokopy <58863283+Proko8@users.noreply.github.com>
2020-09-08 12:39:14 -0400

    08sept

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const pieces = d3.select(".body");
 const svg = pieces.append("svg").attr("height", "800px").attr("width", "800px");
 
 const gameSettings = {
-  height: 800 /*- margin.top - margin.bottom,*/,
-  width: 800 /*- margin.left - margin.right,*/,
+  height: 800,
+  width: 800,
   padding: 20,
-  enemies: 20,
+  nEnemies: 20,
 };
 
 const scoreBoard = {
@@ -14,60 +14,21 @@ const scoreBoard = {
   best: 0,
 };
 
-// const totalEnemies = svg
-
-// function allEnemies(enemies) {
-//   let i = 0;
-//   while (i < enemies) {
-//     totalEnemies
-//       .append("circle")
-//       .attr("cx", 50)
-//       .attr("cy", 50)
-//       .attr("r", 5)
-//       .style("fill", "silver")
-//       .style("stroke", "red")
-//       .style("stroke-width", 1)
-//     i++;
-//   }
-// }
-// allEnemies(gameSettings.enemies);
-
-// totalEnemies.transition()
-//             .duration(1000)
-//             .attr("cx", 400)
-
-var totalEnemies = svg
-  .append("circle")
-  .attr("cx", 400)
-  .attr("cy", 200)
-  .attr("r", 8)
-  .style("fill", "silver")
-  .style("stroke", "red")
-  .style("stroke-width", 1);
-
-totalEnemies
-  .transition()
-  .duration(1000)
-  .attr("cx", Math.random() * 500)
-  .attr("cy", Math.random() * 500);
-
 var player = svg
   .append("circle")
   .attr("cx", 400)
-  .attr("cy", 200)
+  .attr("cy", 400)
   .attr("r", 8)
   .style("fill", "yellow")
   .style("stroke", "blue")
   .style("stroke-width", 1);
+player.append("use");
 
-player
-  .transition()
-  .duration(1000)
-  .attr("cx", Math.random() * 500)
-  .attr("cy", Math.random() * 500)
-  // .each("Start", function () {
-  //   d3.select(this);
-  // });
+var dragHandler = d3.drag().on("drag", function () {
+  d3.select(this).attr("cx", d3.event.x).attr("cy", d3.event.y);
+});
+
+dragHandler(svg.selectAll("use"));
 
 const scoreboard = svg
   .append("rect")
@@ -90,3 +51,199 @@ const best = svg
   .text(`High Score : ${scoreBoard.best}`)
   .attr("x", 696)
   .attr("y", 40);
+
+
+  var totalEnemies = svg
+  .append("circle")
+  .attr("cx", 0)
+  .attr("cy", 0)
+  .attr("r", 8)
+  .style("fill", "silver")
+  .style("stroke", "red")
+  .style("stroke-width", 1)
+  .transition()
+  .duration(1000)
+  .attr("cx", Math.random() * 790)
+  .attr("cy", Math.random() * 790)
+  .end();
+
+  var totalEnemies = svg
+  .append("circle")
+  .attr("cx", 0)
+  .attr("cy", 0)
+  .attr("r", 8)
+  .style("fill", "silver")
+  .style("stroke", "red")
+  .style("stroke-width", 1)
+  .transition()
+  .duration(1000)
+  .attr("cx", Math.random() * 790)
+  .attr("cy", Math.random() * 790);
+  var totalEnemies = svg
+  .append("circle")
+  .attr("cx", 0)
+  .attr("cy", 0)
+  .attr("r", 8)
+  .style("fill", "silver")
+  .style("stroke", "red")
+  .style("stroke-width", 1)
+  .transition()
+  .duration(1000)
+  .attr("cx", Math.random() * 790)
+  .attr("cy", Math.random() * 790);
+  var totalEnemies = svg
+  .append("circle")
+  .attr("cx", 0)
+  .attr("cy", 0)
+  .attr("r", 8)
+  .style("fill", "silver")
+  .style("stroke", "red")
+  .style("stroke-width", 1)
+  .transition()
+  .duration(1000)
+  .attr("cx", Math.random() * 790)
+  .attr("cy", Math.random() * 790);
+  var totalEnemies = svg
+  .append("circle")
+  .attr("cx", 0)
+  .attr("cy", 0)
+  .attr("r", 8)
+  .style("fill", "silver")
+  .style("stroke", "red")
+  .style("stroke-width", 1)
+  .transition()
+  .duration(1000)
+  .attr("cx", Math.random() * 790)
+  .attr("cy", Math.random() * 790);
+  var totalEnemies = svg
+  .append("circle")
+  .attr("cx", 0)
+  .attr("cy", 0)
+  .attr("r", 8)
+  .style("fill", "silver")
+  .style("stroke", "red")
+  .style("stroke-width", 1)
+  .transition()
+  .duration(1000)
+  .attr("cx", Math.random() * 790)
+  .attr("cy", Math.random() * 790);
+  var totalEnemies = svg
+  .append("circle")
+  .attr("cx", 0)
+  .attr("cy", 0)
+  .attr("r", 8)
+  .style("fill", "silver")
+  .style("stroke", "red")
+  .style("stroke-width", 1)
+  .transition()
+  .duration(1000)
+  .attr("cx", Math.random() * 790)
+  .attr("cy", Math.random() * 790);
+  var totalEnemies = svg
+  .append("circle")
+  .attr("cx", 0)
+  .attr("cy", 0)
+  .attr("r", 8)
+  .style("fill", "silver")
+  .style("stroke", "red")
+  .style("stroke-width", 1)
+  .transition()
+  .duration(1000)
+  .attr("cx", Math.random() * 790)
+  .attr("cy", Math.random() * 790);
+  var totalEnemies = svg
+  .append("circle")
+  .attr("cx", 0)
+  .attr("cy", 0)
+  .attr("r", 8)
+  .style("fill", "silver")
+  .style("stroke", "red")
+  .style("stroke-width", 1)
+  .transition()
+  .duration(1000)
+  .attr("cx", Math.random() * 790)
+  .attr("cy", Math.random() * 790);
+  var totalEnemies = svg
+  .append("circle")
+  .attr("cx", 0)
+  .attr("cy", 0)
+  .attr("r", 8)
+  .style("fill", "silver")
+  .style("stroke", "red")
+  .style("stroke-width", 1)
+  .transition()
+  .duration(1000)
+  .attr("cx", Math.random() * 790)
+  .attr("cy", Math.random() * 790);
+  var totalEnemies = svg
+  .append("circle")
+  .attr("cx", 0)
+  .attr("cy", 0)
+  .attr("r", 8)
+  .style("fill", "silver")
+  .style("stroke", "red")
+  .style("stroke-width", 1)
+  .transition()
+  .duration(1000)
+  .attr("cx", Math.random() * 790)
+  .attr("cy", Math.random() * 790);
+  var totalEnemies = svg
+  .append("circle")
+  .attr("cx", 0)
+  .attr("cy", 0)
+  .attr("r", 8)
+  .style("fill", "silver")
+  .style("stroke", "red")
+  .style("stroke-width", 1)
+  .transition()
+  .duration(1000)
+  .attr("cx", Math.random() * 790)
+  .attr("cy", Math.random() * 790);
+  var totalEnemies = svg
+  .append("circle")
+  .attr("cx", 0)
+  .attr("cy", 0)
+  .attr("r", 8)
+  .style("fill", "silver")
+  .style("stroke", "red")
+  .style("stroke-width", 1)
+  .transition()
+  .duration(1000)
+  .attr("cx", Math.random() * 790)
+  .attr("cy", Math.random() * 790);
+  var totalEnemies = svg
+  .append("circle")
+  .attr("cx", 0)
+  .attr("cy", 0)
+  .attr("r", 8)
+  .style("fill", "silver")
+  .style("stroke", "red")
+  .style("stroke-width", 1)
+  .transition()
+  .duration(1000)
+  .attr("cx", Math.random() * 790)
+  .attr("cy", Math.random() * 790);
+  var totalEnemies = svg
+  .append("circle")
+  .attr("cx", 0)
+  .attr("cy", 0)
+  .attr("r", 8)
+  .style("fill", "silver")
+  .style("stroke", "red")
+  .style("stroke-width", 1)
+  .transition()
+  .duration(1000)
+  .attr("cx", Math.random() * 790)
+  .attr("cy", Math.random() * 790);
+  var totalEnemies = svg
+  .append("circle")
+  .attr("cx", 0)
+  .attr("cy", 0)
+  .attr("r", 8)
+  .style("fill", "silver")
+  .style("stroke", "red")
+  .style("stroke-width", 1)
+  .transition()
+  .duration(1000)
+  .attr("cx", Math.random() * 790)
+  .attr("cy", Math.random() * 790);
